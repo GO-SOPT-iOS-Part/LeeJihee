2157719578ca6d16c585afd20f146ff2a0c921fe
Zoe <Zoe.urssu@gmail.com>
2023-04-06 04:19:32 +0900

    [Week1] TODO 추가

--- SOPT_iOS/SOPT_iOS/SceneDelegate.swift
+++ SOPT_iOS/SOPT_iOS/SceneDelegate.swift
@@ -11,7 +11,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    
+    //TODO:  window와 windowScene이 어떤 역할을 하는지 공부하기!
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
             
             guard let windowScene = (scene as? UIWindowScene) else { return }
